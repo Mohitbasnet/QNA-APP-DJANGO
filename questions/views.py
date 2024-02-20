41df3ca1288bae0f7ae8e659fc74edf701dcb975
banset <mohitbasnet80@gmail.com>
2024-02-20 14:05:57 +0545

    Added Qdetial page

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from . models import Question
 # Create your views here.
 def question_list(request):
     question_list = Question.objects.all().order_by('-created_at')
     return render(request,"Qlist.html",{'question_list': question_list})
+
+def question_details(request,slug):
+    question = Question.objects.get(slug = slug)
+    return render(request,"Qdetails.html",{"question":question})
