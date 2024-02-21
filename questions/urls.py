7a968eb70528309a9dd052b260d61e1d0f9a17a7
banset <mohitbasnet80@gmail.com>
2024-02-21 13:34:08 +0545

    Added registration functionality

--- questions/urls.py
+++ questions/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from . views import question_list,question_details
+from . views import question_list,question_details,register
 
 urlpatterns = [
     path("question/", question_list, name = "question-list"),
     path("question/<slug:slug>/",question_details, name = "question-details"),
+    path('register/',register,name = "register"),
 ]
